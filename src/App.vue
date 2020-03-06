e629ec2246ec0adc552b2ff03d3090a3fbdb0359
zsstrike <2439759713@qq.com>
2020-03-06 14:37:46 +0800

    SideBar done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="navbar">
+      <SideBar/>
     </div>
-    <router-view/>
+    <div id="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
+<script>
+import SideBar from './components/SideBar'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
 <style>
+
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  font-size: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
+#navbar {
+  display: inline-block;
+  width: 20%;
+  height: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#content {
+  display: inline-block;
+  height: 100%;
+  width: 80%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="side-bar">
+    <router-link to="/">
+      Server
+    </router-link>
+    <router-link to="/about">
+      Add
+    </router-link>
+    <router-link to="/setting">
+      Setting
+    </router-link>
+    <router-link to="/preference">
+      Preference
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+#side-bar {
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  font-size: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+a {
+  display: inline-block;
+  font-size: 1.1rem;
+  color: #232323;
+  text-decoration: none;
+  width: 100%;
+  height: 3rem;
+  line-height: 3rem;
+  text-align: center;
+}
+a:hover {
+  background: #ededed;
+}
+a.router-link-exact-active {
+  color: cadetblue;
+  background: #ededed;
+}
+</style>
